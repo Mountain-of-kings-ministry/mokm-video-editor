89ab3767b5c1f84771db2cc18eadb9edb524ed28
mernking <davtekdon@gmail.com>
2026-04-26 14:16:49 +0100

    update : fixing ghpage 404 error for assets

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  ssr: false, // Required for GitHub Pages (SPA Mode)
+  basename: "/mokm-video-editor/", // Tells the router where it lives
+
+  // Only change appDirectory if your source code is actually inside a folder with this name
+  // If your code is in web/app, use "app"
+  appDirectory: "app",
 } satisfies Config;
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,22 @@
+// import { reactRouter } from "@react-router/dev/vite";
+// import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [tailwindcss(), reactRouter()],
+//   resolve: {
+//     tsconfigPaths: true,
+//   },
+// });
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  // This is the CRITICAL fix for Asset 404s
+  base: "/mokm-video-editor/",
+
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
